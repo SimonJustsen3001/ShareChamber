11de8bc1498f6f7bff93549330e856bcd4ac2907
JustsenSimon <justsensimon@hotmail.com>
2023-03-23 13:23:48 +0100

    You can now Register and Login to the app

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,27 +1,17 @@
+using API.DTOs;
 using API.Services;
-using Application.Handlers;
-using AutoMapper;
 using Domain;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-  public class AccountController : ApiBaseController
+  [ApiController]
+  [Route("api/[controller]")]
+  public class AccountController : ControllerBase
   {
-    private static List<AppUser> users = new List<AppUser>
-    {
-      new AppUser {
-        DisplayName = "simon",
-        UserName = "simon",
-        Email = "simon@hotmail.com"
-      },
-      new AppUser {
-        DisplayName = "jesper",
-        UserName = "jesper",
-        Email = "jesper@hotmail.com"
-      }
-    };
     private readonly ILogger<AccountController> _logger;
     private readonly TokenService _tokenService;
     private readonly UserManager<AppUser> _userManager;
@@ -33,14 +23,82 @@ namespace API.Controllers
       _logger = logger;
     }
 
-    // [HttpPost("login")]
-    // public async Task<IActionResult> Login(AppUser user)
-    // {
-    //   _logger.LogInformation("Test", DateTime.UtcNow);
-    //   return HandleResult(await Mediator.Send(new Login.Command { AppUser = user }));
-    // }
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+
+      var user = await _userManager.FindByEmailAsync(loginDto.Email);
+      if (user == null)
+        return Unauthorized();
+
+      var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+
+      if (result)
+      {
+        return CreateUserObject(user);
+      }
+
+      return Unauthorized();
+
+
+
+    }
+
+    [AllowAnonymous]
+    [HttpPost("register")]
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+      var validationProblem = false;
+
+      if (CheckIfExists(registerDto, "username", "Username is taken", (x) => x.UserName == registerDto.UserName))
+        validationProblem = true;
+      if (CheckIfExists(registerDto, "email", "Email is taken", (x) => x.Email == registerDto.Email))
+        validationProblem = true;
+
+      if (validationProblem) return ValidationProblem(ModelState);
+
+      var user = CreateAppUser(registerDto);
+
+      var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+      if (result.Succeeded)
+      {
+        return CreateUserObject(user);
+      }
+
+      return BadRequest("Registration failed");
+    }
+
+    private UserDto CreateUserObject(AppUser user)
+    {
+      return new UserDto
+      {
+        DisplayName = user.DisplayName,
+        Token = _tokenService.CreateToken(user),
+        UserName = user.UserName
+      };
+    }
+
+    private AppUser CreateAppUser(RegisterDto registerDto)
+    {
+      return new AppUser
+      {
+        DisplayName = registerDto.DisplayName,
+        Email = registerDto.Email,
+        UserName = registerDto.UserName
+      };
+    }
+
+    private bool CheckIfExists(RegisterDto registerDto, string error, string errorMessage, Func<AppUser, Boolean> query)
+    {
+      if (_userManager.Users.Any(query))
+      {
+        ModelState.AddModelError(error, errorMessage);
+        return true;
+      }
+      return false;
+    }
+
   }
 }
-
-//_logger.LogInformation("Successfully returned users", DateTimeOffset.UtcNow);
-//return Ok(users.Select(user => _mapper.Map<LoginDto>(user)));
--- API/DTOs/LoginDto.cs
+++ API/DTOs/LoginDto.cs
@@ -2,8 +2,7 @@ namespace API.DTOs
 {
   public class LoginDto
   {
-    public string DisplayName { get; set; }
-    public string Token { get; set; }
-    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
   }
 }
