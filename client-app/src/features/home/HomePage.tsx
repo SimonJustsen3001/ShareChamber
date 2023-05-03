ce6a9344ba27306cbb2d8f5cbe4b7e95270b3561
JustsenSimon <justsensimon@hotmail.com>
2023-05-03 13:32:49 +0200

    added some more content to the homepage to test with mobile design

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -17,9 +17,9 @@ const HomePage = observer(() => {
         scrollTrigger: {
           trigger: slider.current,
           pin: true,
-          scrub: 1,
+          scrub: true,
           snap: 1 / (panels.length - 1),
-          end: () => "+=" + slider.current?.offsetWidth,
+          end: () => `+=${slider.current!.offsetWidth / 4}`,
           markers: true,
         },
       });
@@ -29,26 +29,32 @@ const HomePage = observer(() => {
 
   return (
     <div ref={component}>
-      <div className="firstContainer">
-        <h1>Testing horizontal scrolling w/ three sections</h1>
-        <h2>First Container</h2>
-      </div>
       <div ref={slider} className="container">
         <div className="panel panel-1">
-          <div>
-            SCROLL DOWN
-            <div className="scroll-down">
-              <div className="arrow"></div>
+          <div className="information-container">
+            <div className="about-container">
+              <div className="about-name">Sharechamber</div>
+              <div className="about-text">Make movie lists together</div>
+            </div>
+            <div className="scrollable-container">
+              <img
+                className="scrollable-image"
+                src="https://m.media-amazon.com/images/M/MV5BNmM4YTFmMmItMGE3Yy00MmRkLTlmZGEtMzZlOTQzYjk3MzA2XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
+              />
+              <img
+                className="scrollable-image"
+                src="https://m.media-amazon.com/images/M/MV5BMjlmZmI5MDctNDE2YS00YWE0LWE5ZWItZDBhYWQ0NTcxNWRhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
+              />
             </div>
           </div>
         </div>
         <div className="panel panel-2">ONE</div>
         <div className="panel panel-3">TWO</div>
-        <div className="panel panel-4">THREE</div>
       </div>
-      <div className="lastContainer">Last Container</div>
     </div>
   );
 });
 
 export default HomePage;
+
+// slider.current?.offsetWidth
